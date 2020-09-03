523b9887ccbe17bc3047e176cbcd4134ff50dd54
wujianming <wujianming@inke.cn>
2020-09-03 20:15:04 +0800

    feat: 1、删除不必要的包 2、添加说明

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ npm run dev
 1. 访问 localhost:9191/ 
 2. 点击跳转preview 跳转到preview页，发现componentDidMount执行了多次
 
-事实上getInitialProps也执行了多次，不过在这个最小demo里面没复现
+事实上getInitialProps也执行了多次，不过在这个最小demo里面没复现,这个demo只能看到第一次路由跳转发生了多次请求，后续返回再进入ok，不过在项目中是多次进入preview都会执行多次。
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,18 +3,19 @@ import { Link } from 'umi';
 
 export default class Index extends Component {
   static getInitialProps = ctx => {
-    console.log('Index getInitialProps')
+    console.log('Index getInitialProps');
   };
 
   componentDidMount() {
-    console.log('Index componentDidMount')
+    console.log('Index componentDidMount');
   }
 
   render() {
     return (
       <div>
-        Index<br/>
-        <Link to="/preview">go to preview</Link>
+        Index
+        <br />
+        <Link to="/preview">点击跳转到preview</Link>
       </div>
     );
   }
